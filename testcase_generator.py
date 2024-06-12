7262af5d0104cd01d013f08ea9a511c89358b203
th.hamed75@gmail.com <th.hamed75@gmail.com>
2024-06-12 17:08:00 -0400

    adding testcase generator module

--- testcase_generator.py
+++ testcase_generator.py
@@ -1,18 +1,16 @@
-import os
 from dotenv import load_dotenv
-import json
+
 load_dotenv()
 from humaneval_loader import HumanEvalLoader
-from LanguageAgentTreeSearch.programming.generators import generator_factory, model_factory
+from TestcaseGenerator.generators import generator_factory, model_factory
 import time
 import pickle
 from tqdm import tqdm
 from MBPPLoader import MBPPLoader
 
+
 def generate_for_mbpp():
-    mbpp_random_instances = json.loads(os.getenv('mbpp_random_instances'))
-    print(mbpp_random_instances)
-    mbpp_loader = MBPPLoader(mbpp_random_instances)
+    mbpp_loader = MBPPLoader(None)
     prompts = mbpp_loader.get_prompts()
 
     gen = generator_factory("python")
@@ -20,15 +18,16 @@ def generate_for_mbpp():
     number_of_tests = 10
     generated_tests = []
     time1 = time.time()
-    for instance in prompts:
+    print(len(prompts))
+    for instance in tqdm(prompts):
         tests_i = gen.internal_tests(instance, test_model, number_of_tests)
-        print(tests_i)
         generated_tests.append(tests_i)
     with open('testcases/mbpp_generated_testcases', 'wb') as fp:
         pickle.dump(generated_tests, fp)
     print(len(generated_tests))
     time2 = time.time()
     print("total_time:", time2 - time1)
+
 
 def generate_for_humaneval():
     gen = generator_factory("python")
@@ -40,12 +39,12 @@ def generate_for_humaneval():
     for idx, item in tqdm(enumerate(human_eval)):
         tests_i = gen.internal_tests(item["prompt"], test_model, number_of_tests)
         human_eval_testcases.append(tests_i)
+        print(tests_i)
     time2 = time.time()
     print(human_eval_testcases)
     with open('testcases/humaneval_generated_testcases', 'wb') as fp:
         pickle.dump(human_eval_testcases, fp)
     print("total_time:", time2 - time1)
-
 
 
 if __name__ == "__main__":
