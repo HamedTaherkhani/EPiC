974e22ac0666afa030d1558735c0f275c83f3b6a
th.hamed75@gmail.com <th.hamed75@gmail.com>
2024-06-20 16:40:04 -0400

    some refactorings

--- experiments.py
+++
@@ -1,9 +0,0 @@
-import os
-import transformers
-import torch
-
-os.environ['TRANSFORMERS_CACHE'] = '/home/hamedth/projects/def-hemmati-ac/hamedth/hugging_face'
-
-print(torch.cuda.get_device_name(1))
-print(torch.cuda.device_count())
-
--- generate_first_population.py
+++ generate_first_population.py
@@ -15,14 +15,14 @@ def get_first_population(gpt_prompts, human_eval, population_size, idx):
     return b
 
 
-def generate_first_population_for_instance(prompt, population_size, idx, client, human_eval, use_stored_prompts=True):
+def generate_first_population_for_instance(prompt, population_size, idx, client, human_eval, dataset_choice,generated_testcases,use_stored_prompts=True):
     if use_stored_prompts:
         if population_size == 5:
             prompt = get_gpt_prompts_distilled()[idx]
         else:
             prompt = get_gpt_prompts_distilled_ten()[idx]
     else:
-        prompt = generate_first_population(prompt, population_size, client)
+        prompt = generate_first_population(prompt, population_size, client, dataset_choice, generated_testcases)
     first_generation = refactor_prompt(get_first_population(prompt, human_eval, population_size, idx))
     return first_generation
 
@@ -38,29 +38,54 @@ def generate_first_population_for_instance_v2(prompt, population_size, client):
     return refactor_prompt(first_generation)
 
 
-def get_completion(client, prompt, population_size):
-    response = client.chat.completions.create(
-        model=openai_model,
-        messages=[{"role": "user", "content":
-            """Please rewrite the function description based on these instructions:
+def get_completion(client, prompt, population_size, dataset_choice=1):
+    if dataset_choice == 1:
+        prompt1 = """Please rewrite the function description based on these instructions:
             1- Add input and output types of the function to the description.
             2- Elaborate the description so that it is understandable for large language models.
-            3- Explore if there are any exceptional or edge cases and elaborate on them. Ignore input validation and focus on cases which clarifies the problem.
-            4- Keep the original testcases and add enough test cases to the description to cover the edge cases. Do not separate the generated testcases and the original ones.
-            Keep the structure of the function and add the description as a comment in the function. Use at most 800 words. Do not implement the code\n""" +
-            prompt}],
-        temperature=0.5,
-        max_tokens=1000,
+            3- Keep the original testcases and add enough test cases to the description to cover the edge cases. Do not separate the generated testcases and the original ones.
+            Keep the structure of the function and add the description as a comment in the function. Use at most 500 words. Do not implement the code\n"""
+        max_tokens = 600
+    else:
+        prompt1 = """Please rewrite the function description based on these instructions:
+            1- Add input and output types of the function to the description.
+            2- Elaborate the description so that it is understandable for large language models.
+            Keep the structure of the function and add the description as a comment in the function. Use at most 250 words. Do not implement the code\n"""
+        max_tokens = 400
+    response = client.chat.completions.create(
+        model=openai_model,
+        messages=[{"role": "user", "content": prompt1+prompt}],
+        temperature=0.9,
+        max_tokens=max_tokens,
         n=population_size
     )
     return response
 
 
-def generate_first_population(prompt, population_size, client):
-    response = get_completion(client, prompt, population_size)
+def generate_first_population(prompt, population_size, client, dataset_choice, generated_testcases):
+    """
+    :param dataset_choice: 1:humnaeval, 2:mbpp
+    :param generated_testcases:
+    :param prompt:
+    :param population_size:
+    :param client:
+    :return:
+    """
+    response = get_completion(client, prompt, population_size, dataset_choice)
     first_generation = []
     for a_prompt in response.choices:
-        first_generation.append(a_prompt.message.content.split("```")[1].replace('python\n', ''))
+        try:
+            prompt = a_prompt.message.content.split("```")[1].replace('python\n', '')
+        except IndexError:
+            print("index error")
+            print(a_prompt.message.content)
+            prompt = a_prompt.message.content
+        prompt_splits = prompt.split('"""')
+        test_text = ""
+        for a_test in generated_testcases[:3]:
+            test_text += "- " + a_test + " \n"
+        prompt_splits[1] = prompt_splits[1] + "\nTestcases:\n" + test_text
+        first_generation.append('"""'.join(prompt_splits))
 
     return first_generation
 
